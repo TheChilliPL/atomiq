3b11b20f494e289eafdb66a50484a7717047f721
Chilli <chilli@thechilli.dev>
2025-01-02 14:56:33 +0100

    Add Option<&CancellationToken> convenience methods

--- src/cancellation_token.rs
+++ src/cancellation_token.rs
@@ -1,3 +1,4 @@
+use core::ops::Deref;
 use crate::prelude::*;
 
 /// A token that can be used to cancel an async operation.
@@ -12,23 +13,68 @@ impl CancellationToken {
     pub fn new() -> Self {
         Self::default()
     }
+}
 
+trait Cancel {
     /// Cancels the operation associated with the token.
+    ///
+    /// If already cancelled, does nothing.
+    fn cancel(&self);
+    
+    /// Cancels the operation associated with the token and returns `true` if the operation was
+    /// already cancelled.
+    /// 
+    fn fetch_cancel(&self) -> bool;
+    /// Returns `true` if the token has been cancelled.
+    fn is_cancelled(&self) -> bool;
+}
+
+impl Cancel for CancellationToken {
+    fn cancel(&self) {
+        self.0.store(true, Ordering::Relaxed);
+    }
+    
+    fn fetch_cancel(&self) -> bool {
+        self.0.swap(true, Ordering::Relaxed)
+    }
+    
+    fn is_cancelled(&self) -> bool {
+        self.0.load(Ordering::Relaxed)
+    }
+}
+
+impl <T: Deref<Target = CancellationToken>> Cancel for Option<T> {
+    /// Tries to cancel the operation associated with the token.
     /// 
     /// If already cancelled, does nothing.
-    pub fn cancel(&self) {
-        self.0.store(true, Ordering::Relaxed);
+    /// If the token is `None`, does nothing.
+    fn cancel(&self) {
+        if let Some(token) = self {
+            token.cancel();
+        }
     }
     
     /// Cancels the operation associated with the token and returns `true` if the operation was
     /// already cancelled.
-    pub fn fetch_cancel(&self) -> bool {
-        self.0.swap(true, Ordering::Relaxed)
+    /// 
+    /// If the token is `None`, always returns `false`.
+    fn fetch_cancel(&self) -> bool {
+        if let Some(token) = self {
+            token.fetch_cancel()
+        } else {
+            false
+        }
     }
 
     /// Returns `true` if the token has been cancelled.
-    pub fn is_cancelled(&self) -> bool {
-        self.0.load(Ordering::Relaxed)
+    /// 
+    /// If the token is `None`, always returns `false`.
+    fn is_cancelled(&self) -> bool {
+        if let Some(token) = self {
+            token.is_cancelled()
+        } else {
+            false
+        }
     }
 }
 
@@ -43,5 +89,27 @@ mod tests {
 
         token.cancel();
         assert!(token.is_cancelled());
+    }
+    
+    #[test]
+    fn test_option_cancellation_token_some() {
+        let token = CancellationToken::new();
+        
+        let opt_token = Some(&token);
+        
+        assert!(!opt_token.is_cancelled());
+        assert!(!opt_token.fetch_cancel());
+        assert!(opt_token.is_cancelled());
+        assert!(opt_token.fetch_cancel());
+    }
+    
+    #[test]
+    fn test_option_cancellation_token_none() {
+        let opt_token: Option<&CancellationToken> = None;
+        
+        assert!(!opt_token.is_cancelled());
+        assert!(!opt_token.fetch_cancel());
+        assert!(!opt_token.is_cancelled());
+        assert!(!opt_token.fetch_cancel());
     }
 }
